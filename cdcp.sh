4f24e0c96269ee826a316416259e3cb2ef2c6d64
Lucas Osse <lucaskindler@gmail.com>
2017-09-06 10:11:15 -0300

    automated backup process

--- cdcp.sh
+++ cdcp.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
 #get info on disk
-isoinfo -d -i /dev/cdrom | grep -i -E 'block size|volume size' 
+#isoinfo -d -i /dev/cdrom | grep -i -E 'block size|volume size' 
+
+echo -n "Enter your desired iso name and press [ENTER]: "
+read iso_name
 
 # put this on one variable Logical block size is: 2048
 # this on another Volume size is: 327867
 
-#block_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'block size')
-#volume_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'volume size')
+#find information with grep, take numbers from information and removes newlines
+block_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'block size' | grep -o '[0-9*]' | tr -d '\n')
+volume_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'volume size' | grep -o '[0-9*]' | tr -d '\n')
 
-dd if=/dev/cdrom of=test.iso bs=$block_size count=$volume_size
+#for debugging
+#echo dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size
 
+dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size
 
-#2. Running dd with the parameters for block size and volume size:
-echo 'dd if=/dev/cdrom of=test.iso bs=<block size from above> count=<volume size from above>'
+eject
 
 #open tray
-#eject 
+#echo "Do you wish to open the disk tray?"
+#select yn in "Yes" "No"; do
+#    case $yn in
+#        Yes ) eject; break;;
+#        No ) exit;;
+#    esac
+#done 
 
 #close it
-#eject -t
+#echo "Do you wish to close the disk tray?"
+#select yn in "Yes" "No"; do
+#    case $yn in
+#        Yes ) eject -t; break;;
+#        No ) exit;;
+#    esac
+#done
