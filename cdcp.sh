35104cc6862369912af3e61d09783d1f6488f704
Lucas Osse <lucaskindler@gmail.com>
2017-09-13 16:11:20 -0300

    option to restart script, cd checker and loading bar added

--- cdcp.sh
+++ cdcp.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 
 #get info on disk
-#isoinfo -d -i /dev/cdrom | grep -i -E 'block size|volume size' 
+#isoinfo -d -i /dev/cdrom | grep -i -E 'block size|volume size'
 
-echo -n "Enter your desired iso name and press [ENTER]: "
-read iso_name
+#loop so program may restart if user wishes new backup
+while : ; do
 
-# put this on one variable Logical block size is: 2048
-# this on another Volume size is: 327867
+  #checks if there's a cd inserted
+  blkid /dev/cdrom/
 
-#find information with grep, take numbers from information and removes newlines
-block_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'block size' | grep -o '[0-9*]' | tr -d '\n')
-volume_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'volume size' | grep -o '[0-9*]' | tr -d '\n')
+  #if not, warn user and open tray
+  if [ $? -eq 2 ]; then
+    echo "Please insert a disk"
+    eject
+  fi
 
-#for debugging
-#echo dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size
+  echo -n "Enter your desired iso name and press [ENTER]: "
+  read iso_name
 
-dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size
+  # put this on one variable Logical block size is: 2048
+  # this on another Volume size is: 327867
 
-#to add: error handling
-echo "Operation concluded succesfully!"
+  #find information with grep, take numbers from information and removes newlines
+  block_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'block size' | grep -o '[0-9*]' | tr -d '\n')
+  volume_size=$(isoinfo -d -i /dev/cdrom | grep -i -E 'volume size' | grep -o '[0-9*]' | tr -d '\n')
 
-while true; do
-    read -p "Do you wish to eject disk? " yn
-    case $yn in
-        [Yy]* ) eject; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+  #for debugging
+  #echo dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size
 
-while true; do
-    read -p "Do you wish to close tray? " yn
-    case $yn in
-        [Yy]* ) eject -t; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+  dd if=/dev/cdrom of=$iso_name.iso bs=$block_size count=$volume_size status=none & PID=$! #simulate a long process
 
-#open tray
-#echo "Do you wish to open the disk tray?"
-#select yn in "Yes" "No"; do
-#    case $yn in
-#        Yes ) eject; break;;
-#        No ) exit;;
-#    esac
-#done 
+  printf "Running backup..."
 
-#close it
-#echo "Do you wish to close the disk tray?"
-#select yn in "Yes" "No"; do
-#    case $yn in
-#        Yes ) eject -t; break;;
-#        No ) exit;;
-#    esac
-#done
+  printf "["
+  # While process is running...
+  while kill -0 $PID 2> /dev/null; do
+      printf  "#"
+      sleep 3
+  done
+  printf "]\n"
+
+  #to add: error handling
+  printf "Operation concluded succesfully!\n"
+
+  while true; do
+      read -p "Do you want to eject disk? " yn
+      case $yn in
+          [Yy]* ) eject; break;;
+          [Nn]* ) break;;
+          * ) printf "Please answer yes or no.";;
+      esac
+  done
+
+  while true; do
+      read -p "Backup another disk? " yn
+      case $yn in
+          [Yy]* ) eject -t; break;;
+          [Nn]* ) eject -t; exit;; #exits program
+          * ) printf "Please answer yes or no.";;
+      esac
+  done
+
+  done
